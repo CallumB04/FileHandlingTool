ac308608cf338cc31f596456e273aecfabd933ff
Callum Burgoyne <callumburgoyne04@gmail.com>
2020-10-05 16:04:18 +0100

    Removed cwd parameter for each function

--- Scripts/mainmenu.py
+++ Scripts/mainmenu.py
@@ -3,11 +3,11 @@ from rename import renamefiles
 from delete import deletefiles
 from create import createfiles
 
-def mainmenu(cwd):
+def mainmenu():
     os.system("cls")
 
     print("=============================================================")
-    print(f"What would you like to do in the current directory ({cwd})")
+    print(f"What would you like to do in the current directory ({os.path.dirname(os.getcwd())})")
     print("1. Rename Files")
     print("2. Delete Files")
     print("3. Create Files")
@@ -15,9 +15,9 @@ def mainmenu(cwd):
     print("=============================================================")
     choice = str(input())
 
-    if choice == "1" or choice.upper() == "RENAME" : renamefiles(cwd)
-    elif choice == "2" or choice.upper() == "DELETE" : deletefiles(cwd)
-    elif choice == "3" or choice.upper() == "CREATE" : createfiles(cwd)
+    if choice == "1" or choice.upper() == "RENAME" : renamefiles()
+    elif choice == "2" or choice.upper() == "DELETE" : deletefiles()
+    elif choice == "3" or choice.upper() == "CREATE" : createfiles()
     elif choice == "4" or choice.upper() == "EXIT" : return "exit"
     else: print("--- Not an option! Try again! ---")
 
